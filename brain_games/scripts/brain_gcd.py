38d1dc466f48214c1282704ebfa9261f4e6f3c1b
Наталья Тайгачева <dupist@Air-Natala.bbrouter>
2024-04-09 17:41:29 +0700

    Lint changes"

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -3,10 +3,12 @@ import prompt
 import random
 from brain_games.cli import welcome_user
 
+
 def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
+
 
 def main():
     name = welcome_user()
@@ -24,6 +26,7 @@ def main():
             print(f"'{answer}' is wrong answer ;(."
                   + f" Correct answer was '{result}'. \n"
                   + f"Let's try again, {name}!")
-            
+
+
 if __name__ == '__main__':
     main()
